ca9a3a56eb21af448dfa5f41b1457ce292ecaa92
Hirotoshi Yoshitaka <tyounan.moti@gmail.com>
2016-11-24 11:55:09 +0900

    スペクトラムの８バンド分解機能を追加

---
+++ build/unity/sandbox/Assets/LoopbackAudioSource/examples/scripts/EightBandLevelViewer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EightBandLevelViewer : MonoBehaviour {
+	public GameObject sound;
+	public int band = 0;
+	public Color color = Color.white;
+
+	private LoopbackAudioSource source;
+
+	// Use this for initialization
+	void Start () {
+		source = sound.GetComponent<LoopbackAudioSource>();
+		GetComponent<MeshRenderer>().material.color = color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localScale = (new Vector3(1.0f, source.eightBandLevels[band] * 100.0f, 1.0f));
+	}
+}
--- build/unity/sandbox/Assets/LoopbackAudioSource/scripts/LoopbackAudioSource.cs
+++ build/unity/sandbox/Assets/LoopbackAudioSource/scripts/LoopbackAudioSource.cs
@@ -8,6 +8,9 @@ public class LoopbackAudioSource : MonoBehaviour {
 	// 1: 右チャンネル
 	public int Channel = 0;
 
+	private float[] spectrum = new float[1024];
+	public float[] eightBandLevels = new float[8];
+
 	void Start() {
 		SetParameter();
 
@@ -20,6 +23,19 @@ public class LoopbackAudioSource : MonoBehaviour {
 	void Update()
 	{
 		SetParameter();
+
+		// スペクトラムを8バンドへ分解
+		GetComponent<AudioSource>().GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
+		int index = 0;
+		for (int i = 0; i < 8; ++i) {
+			var nextBand = Mathf.Exp(Mathf.Log(1024) / 8 * (i + 1));
+			var weight = 1.0f / (nextBand - index);
+			var sum = 0.0f;
+			for (; index < nextBand; index++) {
+				sum += spectrum[index];
+			}
+			eightBandLevels[i] = Mathf.Sqrt(weight * sum);
+		}
 	}
 
 	void SetParameter() {
